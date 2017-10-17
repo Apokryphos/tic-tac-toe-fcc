5fa21e3c7b3b3ef438ba11ed07955fd4edf941a5
Tony Slavotinek <apokryphos@users.noreply.github.com>
2017-10-17 16:17:05 -0400

    Changed board and cells to CSS grid layout. Added body and media partial style sheets.

--- src/js/app/components/board.jsx
+++ src/js/app/components/board.jsx
@@ -21,23 +21,23 @@ class Board extends React.Component {
   render() {
     return (
       <div className="board">
-        <div>
-          <div className="board-row">
+        {/* <div> */}
+          {/* <div className="board-row"> */}
             {this.renderCell(0)}
             {this.renderCell(1)}
             {this.renderCell(2)}
-          </div>
-          <div className="board-row">
+          {/* </div> */}
+          {/* <div className="board-row"> */}
             {this.renderCell(3)}
             {this.renderCell(4)}
             {this.renderCell(5)}
-          </div>
-          <div className="board-row">
+          {/* </div> */}
+          {/* <div className="board-row"> */}
             {this.renderCell(6)}
             {this.renderCell(7)}
             {this.renderCell(8)}
-          </div>
-        </div>
+          {/* </div> */}
+        {/* </div> */}
       </div>
     );
   }
